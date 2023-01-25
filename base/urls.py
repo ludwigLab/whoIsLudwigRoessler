d75d3bf1f946e98b35c791ac1c6d338003b9e308
ludwig.roessler@epg.com <ludwig.roessler@epg.com>
2023-01-25 13:24:34 +0100

    Login / logout / register functions Add homepage

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+
+    path('', views.landingpage, name="landingpage"),
+    path('homepage/', views.homepage, name="homepage"),
     #path('room/<str:pk>/', views.room, name="room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,60 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
-def home(request):
+def loginPage(request):
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist!')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'Password is not correct!')
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('landingpage')
+
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
+
+
+
+def landingpage(request):
+    return render(request, 'base/landingpage.html')
+
+
+
+@login_required(login_url='login')
+def homepage(request):
     return render(request, 'base/homepage.html')
