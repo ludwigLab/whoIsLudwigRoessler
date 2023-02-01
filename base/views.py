5a287fe60428fbb8bfec35c0bdda50837c64aad0
ludwig.roessler@epg.com <ludwig.roessler@epg.com>
2023-02-01 15:08:15 +0100

    homepage bootstrap 5 template

--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
 
+
 def loginPage(request):
     page = 'login'
     if request.method == 'POST':
